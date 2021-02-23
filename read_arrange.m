9b29cbdb0988d0775f802b3ee635d780cee9369f
NiklasEdvall <nikedv@compute.(none)>
2021-02-23 20:01:12 +0100

    RS PSD

--- read_arrange.m
+++ read_arrange.m
@@ -1,3 +1,8 @@
 %% Read and arrange fif-files for TinMEG1
 
+% Read subjects and dates. V1 = ID, V2 = date as YYMMDD
+
+sub_date = readtable('../sub_date.txt')
+meg_data_path = '/archive/20061_tinnitus'
+
 %%
