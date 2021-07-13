f9c2e48988f145597926e5cd7e71e7c093624f27
NiklasEdvall <nikedv@compute.(none)>
2021-07-13 16:49:08 +0200

    loop ICA of ECG over subjects

--- ICA.m
+++ ICA.m
@@ -1,73 +1,74 @@
 
 
 % For every subject
-for i = 1:length(sub_date.ID(1:4)) %test with first 4
-    
-%load and append MEG data
-%infiles = find_files(['../mat_data/preprocessing/' 'ID' sub_date.ID{i}], 'ds_clean');
-%fname = eval(sub_date.ID{i}) ??
+for i = 1:4 %test with first 4, length(sub_date.ID)
+
+inpath = ['../mat_data/preprocessing/' 'ID' sub_date.ID{i} '/']
+
+outdir = ['../mat_data/ICA/' 'ID' sub_date.ID{i} '/']
+
+%Check if subject dir exist, create/define
+if ~exist(outdir, 'file');
+mkdir(outdir);
+end
 
 %Load in the stupidest way possible
 load(['../mat_data/preprocessing/' 'ID' sub_date.ID{i} '/' 'GO_ds_clean.mat']);
 GOmat = cleaned4mat;
-
 load(['../mat_data/preprocessing/' 'ID' sub_date.ID{i} '/' 'PO60_ds_clean.mat']);
 PO60mat = cleaned4mat;
-
 load(['../mat_data/preprocessing/' 'ID' sub_date.ID{i} '/' 'PO70_ds_clean.mat']);
 PO70mat = cleaned4mat;
-
 load(['../mat_data/preprocessing/' 'ID' sub_date.ID{i} '/' 'GP60_ds_clean.mat']);
 GP60mat = cleaned4mat;
-
 load(['../mat_data/preprocessing/' 'ID' sub_date.ID{i} '/' 'GP70_ds_clean.mat']);
 GP70mat = cleaned4mat;
 
+%Append data
 cfg = [];
+cfg.keepsampleinfo = 'no';
 appended = ft_appenddata(cfg, PO60mat, PO70mat, GP60mat, GP70mat, GOmat);
 
-end
-
-
-
+%Identify components and save
 cfg              = [];
 cfg.method       = 'fastica';
 cfg.numcomponent = 40;
 cfg.channel      = 'MEG';
-comp             = ft_componentanalysis(cfg, cleaned4mat);
+comp             = ft_componentanalysis(cfg, appended);
 
-%% paj
+save([outdir 'components.mat'], 'comp', '-v7.3');
 
-cfg = [];
-cfg.layout      = 'neuromag306all.lay';
-cfg.marker      = 'off';
-cfg.component   = [1:20];               
-figure; ft_topoplotIC(cfg, comp);
+% %Plot
+% cfg = [];
+% cfg.layout      = 'neuromag306all.lay';
+% cfg.marker      = 'off';
+% cfg.component   = [1:20];               
+% figure; ft_topoplotIC(cfg, comp);
+% 
+% % Time-series view (split in two figures)
+% cfg = [];
+% cfg.viewmode    = 'component';
+% cfg.layout      = 'neuromag306all.lay';
+% cfg.blocksize   = 10;
+% cfg.channel     = [1:20];
+% ft_databrowser(cfg, comp)
 
-% Time-series view (split in two figures)
-cfg = [];
-cfg.viewmode    = 'component';
-cfg.layout      = 'neuromag306all.lay';
-cfg.blocksize   = 10;
-cfg.channel     = [1:20];
-ft_databrowser(cfg, comp)
-%% Semiauto ECG ICA
-
-%Select QRS threshold and time window
+%Select QRS threshold and time window, requires inspection
 cfg = [];
 cfg.continuous            = 'no';
-cfg.artfctdef.ecg.pretim  = 0.075; %0.3
-cfg.artfctdef.ecg.psttim  = 0.1; %0.3
+cfg.artfctdef.ecg.pretim  = 0.3; %0.3
+cfg.artfctdef.ecg.psttim  = 0.3; %0.3
+cfg.artfctdef.ecg.cutoff  = 3; %Default 3
 cfg.channel               = 'ECG';
 cfg.artfctdef.ecg.inspect = 'ECG';
-[~, artifact] = ft_artifact_ecg(cfg, cleaned4mat);
+[~, artifact] = ft_artifact_ecg(cfg, appended);
 
 % Make artifact epochs
 cfg = [];
 cfg.dftfilter  = 'yes';
 cfg.demean     = 'yes';
 cfg.trl        = [artifact zeros(size(artifact,1), 1)];
-temp = ft_redefinetrial(cfg, cleaned4mat);
+temp = ft_redefinetrial(cfg, appended);
 
 % Re-arrange data
 cfg.channel    = 'MEG*'; %??
@@ -90,22 +91,24 @@ cfg.unmixing  = comp.unmixing;
 cfg.topolabel = comp.topolabel;
 comp_ecg = ft_componentanalysis(cfg, data_ecg);
 
-
-
-
 % append the ecg channel to the data structure;
 comp_ecg = ft_appenddata([], ecg, comp_ecg);
 
-% average the components timelocked to the QRS-complex
-cfg = [];
-timelock = ft_timelockanalysis(cfg, comp_ecg);
+save([outdir 'components_ecg.mat'], 'comp_ecg', '-v7.3');
 
-% Plot
-figure
-subplot(2,1,1); plot(timelock.time, timelock.avg(1,:)); title('ECG')
-subplot(2,1,2); plot(timelock.time, timelock.avg(2:end,:));  title('ICA comp')
+% average the components timelocked to the QRS-complex to inspect
+% cfg = [];
+% timelock = ft_timelockanalysis(cfg, comp_ecg);
 
-%% Find ECG components
+% % Plot
+% figure
+% subplot(2,1,1); plot(timelock.time, timelock.avg(1,:)); title('ECG')
+% subplot(2,1,2); plot(timelock.time, timelock.avg(2:end,:));  title('ICA comp')
+
+
+
+% Find ECG components
+
 % Define cutoff
 cutoff = 0.5;           % Between 0-1 (analogue to a correlation coefficient)
 
@@ -128,8 +131,7 @@ fdcomp = ft_connectivityanalysis(cfg, freq);
 maxcoh = max(fdcomp.cohspctrm, [], 2);
 ecg_comp_idx = find(maxcoh > cutoff);
 
-%%
-
+%Create and save plots of components
 figure;
 subplot(3,1,1); 
 plot(fdcomp.freq, abs(fdcomp.cohspctrm)); hold on
@@ -145,21 +147,86 @@ bp = bar(1:length(maxcoh), diag(maxcoh), 'stacked');
 set(bp(foo),'facecolor','w'); set(bp(ecg_comp_idx),'facecolor','r')
 axis([0.5, length(maxcoh)+0.5, 0, 1]); xlabel('comp'); ylabel('coh');
 
-% View marked component(s)
-cfg = [];
-cfg.channel     = ecg_comp_idx; % components to be plotted
-cfg.viewmode    = 'component';
-cfg.layout      = 'neuromag306all.lay'; % specify the layout file that should be used for plotting
-ft_databrowser(cfg, comp)
+saveas(gcf, [outdir 'comp_figure.pdf'])
 
-%% reject component
+close(1)
 
-%ecg_comp_idx:
-reject_comp = [1, 3, 7, 11];    % Write the index of the components you want to remove
+% % View marked component(s)
+% cfg = [];
+% cfg.channel     = ecg_comp_idx; % components to be plotted
+% cfg.viewmode    = 'component';
+% cfg.layout      = 'neuromag306all.lay'; % specify the layout file that should be used for plotting
+% ft_databrowser(cfg, comp)
+
+
+% reject component
 
 % Remove components
 cfg = [];
-cfg.component   = reject_comp; %ecg_comp_idx
+cfg.component   = ecg_comp_idx; %index of components to remove
 cfg.channel     = 'MEG';
 cfg.updatesens  = 'no'; %
-icacleaned_downsampled_data = ft_rejectcomponent(cfg, comp, cleaned_downsampled_data);
+appended_ICA = ft_rejectcomponent(cfg, comp, appended);
+
+
+%Split conditions and save
+
+%NB, order from ft_appenddata:
+%PO60mat, PO70mat, GP60mat, GP70mat, GOmat
+
+%PO60
+first = 1;
+last = length(PO60mat.trial);
+
+cfg = [];
+cfg.trials = (first:last);
+PO60ica = ft_selectdata(cfg, appended_ICA);
+PO60ica.sampleinfo = PO60mat.sampleinfo;
+
+save([outdir 'PO60ica.mat'], 'PO60ica');
+
+%PO70
+first = last+1;
+last = first+length(PO70mat.trial)-1;
+
+cfg = [];
+cfg.trials = (first:last);
+PO70ica = ft_selectdata(cfg, appended_ICA);
+PO70ica.sampleinfo = PO70mat.sampleinfo;
+
+save([outdir 'PO70ica.mat'], 'PO70ica');
+
+%GP60
+first = last+1;
+last = first+length(GP60mat.trial)-1;
+
+cfg = [];
+cfg.trials = (first:last);
+GP60ica = ft_selectdata(cfg, appended_ICA);
+GP60ica.sampleinfo = GP60mat.sampleinfo;
+
+save([outdir 'GP60ica.mat'], 'GP60ica');
+
+%GP70
+first = last+1;
+last = first+length(GP70mat.trial)-1;
+
+cfg = [];
+cfg.trials = (first:last);
+GP70ica = ft_selectdata(cfg, appended_ICA);
+GP70ica.sampleinfo = GP70mat.sampleinfo;
+
+save([outdir 'GP70ica.mat'], 'GP70ica');
+
+%GO
+first = last+1;
+last = first+length(GOmat.trial)-1;
+
+cfg = [];
+cfg.trials = (first:last);
+GOica = ft_selectdata(cfg, appended_ICA);
+GOica.sampleinfo = GOmat.sampleinfo;
+
+save([outdir 'GOica.mat'], 'GOica');
+
+end
