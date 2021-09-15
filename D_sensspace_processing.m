d6fd12f6924d7396fa350a5a12b2d5dc26a44649
NiklasEdvall <nikedv@compute.(none)>
2021-09-15 15:50:07 +0200

    saving sensor space averages

--- D_sensspace_processing.m
+++ D_sensspace_processing.m
@@ -1,14 +1,14 @@
 
 %Load conditions for subjects, average trials via ft_timelockanalysis
-
 sensspace_avg = struct;
-
+all_avg = struct();
+gravg = struct();
 for i = 1:4%length(sub_date.ID)
     
     subinpath = ['../mat_data/ICA/' 'ID' sub_date.ID{i} '/'];
     sensspace_avg.subjects{i,1} = sub_date.ID{i};
     
-    for ii = 1%:length(conditions)
+    for ii = 1:length(conditions)
     
     tempdat = load([subinpath conditions{ii} 'ica.mat']);
     tempdat = tempdat.([conditions{ii} 'ica']);
@@ -22,7 +22,7 @@ for i = 1:4%length(sub_date.ID)
         cfg = [];
         cfg.covariance = 'yes';
         cfg.covariancewindow = 'prestim';
-        cfg.keeptrials = 'no' %if yes, no avg in output variable "timelockeds"?
+        cfg.keeptrials = 'no'; %if yes, no avg in output variable "timelockeds"?
         cfg.preproc.demean = 'yes';
         cfg.preproc.baselinewindow = [-0.200 -0.100]; %check this
         cfg.preproc.lpfilter = 'yes';
@@ -30,6 +30,10 @@ for i = 1:4%length(sub_date.ID)
         cfg.trials = tempdat.trialinfo == trig(iii);
 
         sensspace_avg.(conditions{ii}){i, iii} = ft_timelockanalysis(cfg, tempdat);
+        
+        all_avg.(conditions{ii}){i, iii} = sensspace_avg.(conditions{ii}){i, iii}.avg;
+        
+        gravg.(conditions{ii}){iii} = mean(cat(3, all_avg.(conditions{ii}){:, iii}), 3);
         
         %For trigs
         end
@@ -40,23 +44,40 @@ for i = 1:4%length(sub_date.ID)
 %For subjects
 end
 
+save('../mat_data/sensspace/sensspace_avg.mat', 'sensspace_avg', '-v7.3');
+save('../mat_data/sensspace/all_avg.mat', 'all_avg', '-v7.3');
+save('../mat_data/sensspace/grand_avg.mat', 'gravg', '-v7.3');
 
-
-grandavg = struct();
-
-for i = 1:2%numel(sub_date.ID)
-   
-    grandavg(i).PO60 = sensspace_avg.PO60{i,5}.avg;
-    
-end
-
+%% MultiplotER
 mean_sub = sensspace_avg.PO60{1,1};
-mean_sub.avg = mean(cat(3, grandavg.PO60), 3);
+mean_sub.avg = gravg.PO60{5};
 
 cfg = [];
 cfg.parameter = 'avg';
 cfg.layout = 'neuromag306mag.lay';
-cfg.linecolor = 'brgkym'
-ft_multiplotER(cfg, sensspace_avg.PO60{1:2,5}, mean_sub);
+cfg.linecolor = [repmat(.5, 4, 3); 1 0 0]; %change according to n of subjects
+ft_multiplotER(cfg, sensspace_avg.PO60{1:4,5}, mean_sub);
 
+%% TopoplotER
+mean_sub = sensspace_avg.PO60{1,1};
 
+    cfg = [];
+    cfg.parameter = 'avg';
+    cfg.layout = 'neuromag306mag.lay';
+    cfg.colorbar = 'no';
+    cfg.comment = '';
+
+    figure;
+    
+for i = 1:numel(cond.PO60label);
+    
+    mean_sub.avg = gravg.PO60{i};
+
+    subplot(2,3,i);
+    ft_topoplotER(cfg, mean_sub);
+    %title(cond.PO60label{i}, 'Interpreter', 'none');
+    
+    ft_hastoolbox('brewermap', 1);         % ensure this toolbox is on the path
+    colormap(flipud(brewermap(64,'RdBu'))) % change the colormap
+
+end
