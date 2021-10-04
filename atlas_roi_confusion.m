7f02521d62964cc45e6b5f753c3ce8b33b832920
NiklasEdvall <nikedv@compute.(none)>
2021-10-04 16:44:58 +0200

    trying to implement atlas ROI

---
+++ atlas_roi_confusion.m
@@ -0,0 +1,41 @@
+%% ROI from atlas
+
+%loads as "pow_diff"
+load('../mat_data/stats/PO60_90_pow_diff.mat');
+
+aal_atlas = ft_read_atlas('../../fieldtrip-20210311/template/atlas/aal/ROI_MNI_V4.nii', 'unit', 'mm')
+
+cfg = [];
+cfg.parameter = 'tissue';
+cfg.interpmethod = 'nearest';
+int_aal_tempgrid = ft_sourceinterpolate(cfg, aal_atlas, template_grid);
+
+int_aal_tempgrid.tissuelabel = aal_atlas.tissuelabel;
+
+atlas_grid = ft_checkdata(int_aal_tempgrid, 'datatype', 'source');
+atlas_grid.inside = template_grid.inside;
+
+pow_diff.tissue = atlas_grid.tissue;
+pow_diff.tissuelabel = atlas_grid.tissuelabel;
+
+%loads as "mri"
+load standard_mri
+
+cfg = [];
+cfg.parameter = 'tissue';
+cfg.interpmethod = 'nearest';
+int_powdiff_mri = ft_sourceinterpolate(cfg, pow_diff, mri);
+
+labs = [81, 82];
+atlas_grid.tissuelabel(labs)
+
+int_powdiff_mri.tissue(~ismember(int_powdiff_mri.tissue, labs)) == 0;
+int_powdiff_mri.tissue(ismember(int_powdiff_mri.tissue, labs)) == 1;
+
+int_powdiff_mri.tissuelabel = {'ROI'};
+
+cfg = [];
+cfg.funparameter = 'tissue';
+cfg.anaparameter = 'anatomy';
+ft_sourceplot(cfg, int_powdiff_mri);
+
