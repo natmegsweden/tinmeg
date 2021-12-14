8e6b26514604befbdd9ce91772ba50306691ecdd
NiklasEdvall <niklasedvall@gmail.com>
2021-12-14 17:16:59 +0100

    more AM frequencies, need to fix plot amp for AM

--- stim/Multi_AM.m
+++ stim/Multi_AM.m
@@ -6,14 +6,20 @@ ptf = 1000;          % Carrier tone frequency (Hz)
 
 totdur = 1;          % total duration of stimuli (sec)
 
-modfreq1 = 8;        % Frequency for amplitude modulation 1
-moddepth1 = 0.5;        % Modulation depth of amplitude modulation 1
+modfreq1 = 2;        % Frequency for amplitude modulation 1
+moddepth1 = 0.2;        % Modulation depth of amplitude modulation 1
 
-modfreq2 = 16;        % Frequency for amplitude modulation 1
-moddepth2 = 0.5;        % Modulation depth of amplitude modulation 1
+modfreq2 = 4;        % Frequency for amplitude modulation 2
+moddepth2 = 0.2;        % Modulation depth of amplitude modulation 2
 
-modfreq3 = 32;        % Frequency for amplitude modulation 1
-moddepth3 = 0.5;        % Modulation depth of amplitude modulation 1
+modfreq3 = 8;        % Frequency for amplitude modulation 3
+moddepth3 = 0.2;        % Modulation depth of amplitude modulation 3
+
+modfreq4 = 16;        % Frequency for amplitude modulation 4
+moddepth4 = 0.2;        % Modulation depth of amplitude modulation 4
+
+modfreq5 = 32;        % Frequency for amplitude modulation 5
+moddepth5 = 0.2;        % Modulation depth of amplitude modulation 5
 
 riset = 0.1;            % rise time at start of stimuli (sec)
 fallt = 0.1;            % fall time at end of stimuli (sec)
@@ -21,6 +27,8 @@ fallt = 0.1;            % fall time at end of stimuli (sec)
 modamp1 = 1;      % Amplitude of modulation 1
 modamp2 = 1;      % Amplitude of modulation 2
 modamp3 = 1;      % Amplitude of modulation 3
+modamp4 = 1;      % Amplitude of modulation 4
+modamp5 = 1;      % Amplitude of modulation 5
 
 t = (0:dt:totdur-dt);   %n samples for total duration
 
@@ -40,15 +48,21 @@ RFenv = [rise ones(1, length(t)-(length(fall)+length(rise))) fall];
 ptam = carrieramp*(1+moddepth1*sin(2*pi*modfreq1*t)) .* sin(2*pi*ptf*t);
 ptam = carrieramp*(1+moddepth2*sin(2*pi*modfreq2*t)) .* ptam;
 ptam = carrieramp*(1+moddepth3*sin(2*pi*modfreq3*t)) .* ptam;
+ptam = carrieramp*(1+moddepth4*sin(2*pi*modfreq4*t)) .* ptam;
+ptam = carrieramp*(1+moddepth5*sin(2*pi*modfreq5*t)) .* ptam;
 ptam = ptam .* RFenv;
+
+%Fix amp-factor for plot (i.e. now fixed at 2000)
 
 figure('Position', [100 100 1200 500]); hold on;
 plot(ptam);
-plot(carrieramp*(1+moddepth2*sin(2*pi*modfreq2*t)))
-plot(carrieramp*(1+moddepth1*sin(2*pi*modfreq1*t)))
-plot(carrieramp*(1+moddepth3*sin(2*pi*modfreq3*t)))
-plot(RFenv)
-legend('Signal (1000 Hz)', 'AM envelope 1 (8 Hz)', 'AM envelope 2 (16 Hz)', 'AM envelope 3 (32 Hz)', 'Rise/Fall envelope')
+plot(2000*sin(2*pi*modfreq1*t))
+plot(2000*sin(2*pi*modfreq2*t))
+plot(2000*sin(2*pi*modfreq3*t))
+plot(2000*sin(2*pi*modfreq4*t))
+plot(2000*sin(2*pi*modfreq5*t))
+plot(2000*RFenv)
+legend('Signal (1000 Hz)', 'AM envelope 1 (2 Hz)', 'AM envelope 2 (4 Hz)', 'AM envelope 3 (8 Hz)', 'AM envelope 4 (16 Hz)', 'AM envelope 5 (32 Hz)', 'Rise/Fall envelope')
 
 %Limit max output to -1/+1
 ptam = ptam/max(abs(ptam(:))); 
