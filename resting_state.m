4aee3ecdec358888eb7d938ba909c6b87f5ba4ae
NiklasEdvall <niklasedvall@gmail.com>
2022-02-08 16:20:49 +0100

    fixed figure widths to fit latex template

--- resting_state.m
+++ resting_state.m
@@ -56,8 +56,6 @@ end
 
 %% Load RS data, clean and create pseudo epochs
 
-
-
 for i = 1:length(sub_date.ID);
 
     outdir = ['../mat_data/preprocessing/' 'ID' sub_date.ID{i} '/'];
@@ -158,30 +156,36 @@ end
 
 %% Plots
 
-figure('Units', 'normalized', 'Position', [0.1 0.1 0.6 0.7]);
-histogram(maxPSDfreq)
-set(gca,'FontSize',14)
-title('Distribution of maximum PSD frequency in Alpha');
-xlabel('Maximum PSD frequency (Hz)');
-ylabel('n count');
-ylim([0 10]);
+load('../output/osc_pwspc.mat');
+load('../output/original_pwspc.mat');
+load('../output/osc_fract.mat');
 
 blue = [0, 0.4470, 0.7410];
 orange = [0.8500, 0.3250, 0.0980];
 
+figure('Units', 'centimeters', 'Position', [5 5 15 10]);
+histogram(maxPSDfreq)
+set(gca,'FontSize',10)
+%title('Distribution of maximum PSD frequency in the Alpha band');
+xlabel('Maximum PSD frequency (Hz)');
+ylabel('n count');
+ylim([0 10]);
+
+saveas(gcf, ['../output/RS_histo.pdf']);
+
 %FoooF demo
-figure('Units', 'normalized', 'Position', [0.1 0.1 0.6 0.7]);
+figure('Units', 'centimeters', 'Position', [5 5 15 12]);
 subplot(2,2,1); hold on;
-set(gca,'FontSize',14)
+set(gca,'FontSize',10)
 plot(freqs, osc_pwspc{2});
 plot(freqs, fract_pwspc{2});
 plot(freqs, original_pwspc{2});
-ylabel('Power Spectrum Density (AU)');
+ylabel('PSD (AU)');
 ylim([-1*10^-26 20*10^-26]);
 xlim([1 30]);
 
 subplot(2,2,2); hold on;
-set(gca,'FontSize',14)
+set(gca,'FontSize',10)
 plot(freqs, osc_pwspc{5});
 plot(freqs, fract_pwspc{5});
 plot(freqs, original_pwspc{5});
@@ -190,17 +194,17 @@ ylim([-1*10^-26 10*10^-25]);
 xlim([1 30]);
 
 subplot(2,2,3); hold on;
-set(gca,'FontSize',14)
+set(gca,'FontSize',10)
 plot(freqs, osc_pwspc{18});
 plot(freqs, fract_pwspc{18});
 plot(freqs, original_pwspc{18});
-ylabel('Power Spectrum Density (AU)');
+ylabel('PSD (AU)');
 xlabel('Frequency (Hz)');
 ylim([-1*10^-26 20*10^-26]);
 xlim([1 30]);
 
 subplot(2,2,4); hold on;
-set(gca,'FontSize',14)
+set(gca,'FontSize',10)
 plot(freqs, osc_pwspc{22});
 plot(freqs, fract_pwspc{22});
 plot(freqs, original_pwspc{22});
@@ -208,9 +212,11 @@ xlabel('Frequency (Hz)');
 ylim([-1*10^-26 20*10^-26]);
 xlim([1 30]);
 
+saveas(gcf, ['../output/RS_subplots.pdf']);
+
 %Summary plot
-figure('Units', 'normalized', 'Position', [0.1 0.3 0.6 0.45]); hold on;
-set(gca,'FontSize',14)
+figure('Units', 'centimeters', 'Position', [5 5 15 8]); hold on;
+set(gca,'FontSize',10)
 for i = [1:10 12:18 20 21 22]
     if i < 22
     plot(freqs, osc_pwspc{i}, 'Color', [0 0 0 0.5], 'HandleVisibility','off')
@@ -222,12 +228,14 @@ end
 plot(freqs, osc_pwspc{18}, 'Color', [0 0 0], 'LineWidth', 1.5)
 plot(freqs, mean(tempmean), 'Color', [0.75 0 0], 'LineWidth', 1.5);
 ylim([-1*10^-25 20*10^-25]);
-xlim([1 30]);
+xlim([1 20]);
 xlabel('Frequency (Hz)');
-ylabel('Power Spectrum Density (AU)');
-legend('Subject 1-21', 'Subject 22', 'Average PSD');
+ylabel('PSD (AU)');
+legend('Subject 1 - 21', 'Subject 22', 'Average PSD');
 
 patch('Faces', [1 2 3 4], 'Vertices', [3.5 -1*10^-25; 3.5 20*10^-25; 7 20*10^-25; 7 -1*10^-25], ...
-'FaceColor', blue, 'FaceAlpha', 0.1, 'EdgeAlpha', 0, 'DisplayName', 'Theta (3.5-7Hz)');
+'FaceColor', blue, 'FaceAlpha', 0.1, 'EdgeAlpha', 0, 'DisplayName', 'Theta (3.5 - 7 Hz)');
 patch('Faces', [1 2 3 4], 'Vertices', [8 -1*10^-25; 8 20*10^-25; 12 20*10^-25; 12 -1*10^-25], ...
-'FaceColor', orange, 'FaceAlpha', 0.1, 'EdgeAlpha', 0, 'DisplayName', 'Alpha (8-12Hz)');
+'FaceColor', orange, 'FaceAlpha', 0.1, 'EdgeAlpha', 0, 'DisplayName', 'Alpha (8 - 12 Hz)');
+
+saveas(gcf, ['../output/RS_average.pdf']);
